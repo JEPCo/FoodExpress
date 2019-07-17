1e44f49cca7bf2dc5fb892982e0d261cb80aae38
Osama AlHurani <49572632+JEPCo@users.noreply.github.com>
2019-07-17 20:25:28 +0300

    update 1

--- app/src/main/java/com/pa/osama/foodexpress/MainActivity.kt
+++ app/src/main/java/com/pa/osama/foodexpress/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.pa.osama.foodexpress
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >22)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.ACCESS_FINE_LOCATION), 123)
+
+        login_new_tv.setOnClickListener {
+            startActivity(Intent(this, SignUpAct::class.java))
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
