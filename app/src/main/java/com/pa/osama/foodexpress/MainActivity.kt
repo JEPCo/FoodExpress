e146f98ca4aa77c455730c37d15e770c53770f7e
Osama AlHurani <49572632+JEPCo@users.noreply.github.com>
2019-07-17 20:21:21 +0300

    Initial Commit

---
+++ app/src/main/java/com/pa/osama/foodexpress/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.pa.osama.foodexpress
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >22)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION), 123)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "شكراً لثقتك بمطعمنا", Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(this,"لا نستطيع الحصول على موقعك لذلك يرجى تحديد موقعك يدوياً بعد هملية الشراء", Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/pa/osama/foodexpress/SignUpAct.kt
@@ -0,0 +1,12 @@
+package com.pa.osama.foodexpress
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
