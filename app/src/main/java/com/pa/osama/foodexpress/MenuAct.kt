631a56dcc271d169a7f9f6030f234738d1b87f96
Osama AlHurani <alhurani93@gmail.com>
2019-07-24 19:13:21 +0300

    update 4

--- app/src/main/java/com/pa/osama/foodexpress/MenuAct.kt
+++ app/src/main/java/com/pa/osama/foodexpress/MenuAct.kt
@@ -1,11 +1,18 @@
 package com.pa.osama.foodexpress
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuAct : AppCompatActivity() {
 
@@ -13,6 +20,25 @@ class MenuAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        var web = AppInfo.url + "get_cat.php"
+        var pd = ProgressDialog(this)
+        pd.setMessage("Please Wait ..")
+        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+        pd.show()
+
+        var rq= Volley.newRequestQueue(this)
+        var jar = JsonArrayRequest(Request.Method.GET, web, null,
+                Response.Listener { response ->
+                    pd.hide()
+                    for (x in 0..response.length()-1)
+                        menu_cat_tabs.addTab(menu_cat_tabs.newTab().setText(response.getJSONObject(x).getString("category")))
+                },
+                Response.ErrorListener { error ->
+                    pd.hide()
+                    Toast.makeText(this, error.message, Toast.LENGTH_LONG).show()
+
+                })
+        rq.add(jar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
